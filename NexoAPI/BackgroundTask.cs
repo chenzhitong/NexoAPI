15a6b160ca17ff0f1247382cf0a32829722d064c
陈志同 <bitcoin2077@outlook.com>
2024-09-23 10:03:01 +0800

    Update BackgroundTask.cs

--- NexoAPI/BackgroundTask.cs
+++ NexoAPI/BackgroundTask.cs
@@ -13,7 +13,6 @@ namespace NexoAPI
     {
         public readonly Logger _logger;
         private readonly NexoAPIContext _context;
-        private bool _isRuning = false;
 
         public BackgroundTask(IServiceScopeFactory _serviceScopeFactory)
         {
@@ -24,173 +23,153 @@ namespace NexoAPI
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            if (_isRuning)
-            {
-                _logger.Info("后台任务已经在运行中");
-                return;
-            }
             _logger.Info("后台任务开始执行");
             while (!stoppingToken.IsCancellationRequested)
             {
                 try
                 {
-                    var temp = _context.User.FirstOrDefault();
-                }
-                catch (Exception ex)
-                {
-                    _logger.Error($"后台任务运行时数据库连接失败 {ex.Message}");
-                    break;
-                }
-                try
-                {
-                    var temp = Helper.Client.GetBlockCountAsync().Result;
-                }
-                catch (Exception ex)
-                {
-                    _logger.Error($"后台任务运行时种子节点连接失败 GetBlockCountAsync");
-                    Task.Delay(TimeSpan.FromSeconds(15), stoppingToken);
-                }
-                _isRuning = true;
+                    //后台任务一：根据用户签名修改交易的RawData
+                    var list1 = _context.Transaction.Include(p => p.Account).Include(p => p.SignResult).ThenInclude(s => s.Signer).
+                        Where(p => p.Status == Models.TransactionStatus.Signing);
 
-                //后台任务一：根据用户签名修改交易的RawData
-                var list1 = _context.Transaction.Include(p => p.Account).Include(p => p.SignResult).ThenInclude(s => s.Signer).
-                    Where(p => p.Status == Models.TransactionStatus.Signing);
-
-                foreach (var tx in list1)
-                {
-                    var rawTx = RpcTransaction.FromJson((JObject)JToken.Parse(tx.RawData), ProtocolSettings.Load(ConfigHelper.AppSetting("Config"))).Transaction;
-
-                    //FeePayer需要单独的签名
-                    var feePayerSignResult = tx.SignResult.FirstOrDefault(p => p.Approved && p.Signer.Address == tx.FeePayer);
-                    if (feePayerSignResult is not null)
+                    foreach (var tx in list1)
                     {
-                        if (!rawTx.Witnesses.Any(p => p.VerificationScript.ToArray().ToHexString() == feePayerSignResult.Signer.GetScript().ToHexString() && p.InvocationScript.Length > 0))
+                        var rawTx = RpcTransaction.FromJson((JObject)JToken.Parse(tx.RawData), ProtocolSettings.Load(ConfigHelper.AppSetting("Config"))).Transaction;
+
+                        //FeePayer需要单独的签名
+                        var feePayerSignResult = tx.SignResult.FirstOrDefault(p => p.Approved && p.Signer.Address == tx.FeePayer);
+                        if (feePayerSignResult is not null)
                         {
-                            using ScriptBuilder scriptBuilder = new();
-                            scriptBuilder.EmitPush(feePayerSignResult.Signature.HexToBytes());
-                            var feePayerWitness = rawTx.Witnesses.FirstOrDefault(p => p.ScriptHash.ToAddress() == feePayerSignResult.Signer.Address);
-                            if (feePayerWitness == null)
-                                _logger.Error($"构造交易时出错，feePayer不在交易的Witness中，TxId = {tx.Hash}, feePayer: {feePayerSignResult.Signer.Address}");
-                            else
-                                feePayerWitness.InvocationScript = scriptBuilder.ToArray();
-                            tx.RawData = rawTx.ToJson(ProtocolSettings.Load(ConfigHelper.AppSetting("Config"))).ToString();
-                            _context.Update(feePayerSignResult);
-                            _context.SaveChanges();
-                        }
-                    }
-                    //如果有additionalSigner，则需要单独的签名
-                    if (!string.IsNullOrEmpty(tx.AdditionalSigner))
-                    {
-                        var additionalSigner = tx.SignResult.FirstOrDefault(p => p.Approved && p.Signer.Address == tx.AdditionalSigner);
-                        if (additionalSigner is not null)
-                        {
-                            if (!rawTx.Witnesses.Any(p => p.VerificationScript.ToArray().ToHexString() == additionalSigner.Signer.GetScript().ToHexString() && p.InvocationScript.Length > 0))
+                            if (!rawTx.Witnesses.Any(p => p.VerificationScript.ToArray().ToHexString() == feePayerSignResult.Signer.GetScript().ToHexString() && p.InvocationScript.Length > 0))
                             {
                                 using ScriptBuilder scriptBuilder = new();
-                                scriptBuilder.EmitPush(additionalSigner.Signature.HexToBytes());
-                                var additionalSignerWitness = rawTx.Witnesses.FirstOrDefault(p => p.ScriptHash.ToAddress() == additionalSigner.Signer.Address);
-                                if (additionalSignerWitness == null)
-                                {
-                                    //_logger.Error($"构造交易时出错，additionalSigner不在交易的Witness中，TxId = {tx.Hash}, additionalSigner: {additionalSigner.Signer.Address}");
-                                    var additionalSignerScript = _context.Account.First(p => p.Address == additionalSigner.Signer.Address).GetScript();
-                                    rawTx.Witnesses = rawTx.Witnesses.Append(new Neo.Network.P2P.Payloads.Witness()
-                                    {
-                                        InvocationScript = null,
-                                        VerificationScript = additionalSignerScript
-                                    }).ToArray();
-                                }
-                                additionalSignerWitness = rawTx.Witnesses.FirstOrDefault(p => p.ScriptHash.ToAddress() == additionalSigner.Signer.Address);
-                                additionalSignerWitness.InvocationScript = scriptBuilder.ToArray();
+                                scriptBuilder.EmitPush(feePayerSignResult.Signature.HexToBytes());
+                                var feePayerWitness = rawTx.Witnesses.FirstOrDefault(p => p.ScriptHash.ToAddress() == feePayerSignResult.Signer.Address);
+                                if (feePayerWitness == null)
+                                    _logger.Error($"构造交易时出错，feePayer不在交易的Witness中，TxId = {tx.Hash}, feePayer: {feePayerSignResult.Signer.Address}");
+                                else
+                                    feePayerWitness.InvocationScript = scriptBuilder.ToArray();
                                 tx.RawData = rawTx.ToJson(ProtocolSettings.Load(ConfigHelper.AppSetting("Config"))).ToString();
-                                _context.Update(additionalSigner);
+                                _context.Update(feePayerSignResult);
                                 _context.SaveChanges();
                             }
                         }
-                    }
-
-                    //签名数满足阈值时，其他用户的签名合并为多签账户的签名
-                    var otherSignResult = tx.SignResult.Where(p => p.Approved && tx.Account.Owners.Contains(p.Signer.Address)).Take(tx.Account.Threshold).ToList();
-                    if (otherSignResult?.Count == tx.Account.Threshold)
-                    {
-                        if (!rawTx.Witnesses.Any(p => p.VerificationScript.ToArray().ToHexString() == tx.Account.GetScript().ToHexString() && p.InvocationScript.Length > 0))
+                        //如果有additionalSigner，则需要单独的签名
+                        if (!string.IsNullOrEmpty(tx.AdditionalSigner))
                         {
-                            using ScriptBuilder scriptBuilder = new();
-                            otherSignResult.OrderBy(p => Neo.Cryptography.ECC.ECPoint.Parse(p.Signer.PublicKey, Neo.Cryptography.ECC.ECCurve.Secp256r1)).ForEach(p => scriptBuilder.EmitPush(p.Signature.HexToBytes()));
-                            rawTx.Witnesses.First(p => p.VerificationScript.ToArray().ToHexString() == tx.Account.GetScript().ToHexString()).InvocationScript = scriptBuilder.ToArray();
-                            tx.RawData = rawTx.ToJson(ProtocolSettings.Load(ConfigHelper.AppSetting("Config"))).ToString();
+                            var additionalSigner = tx.SignResult.FirstOrDefault(p => p.Approved && p.Signer.Address == tx.AdditionalSigner);
+                            if (additionalSigner is not null)
+                            {
+                                if (!rawTx.Witnesses.Any(p => p.VerificationScript.ToArray().ToHexString() == additionalSigner.Signer.GetScript().ToHexString() && p.InvocationScript.Length > 0))
+                                {
+                                    using ScriptBuilder scriptBuilder = new();
+                                    scriptBuilder.EmitPush(additionalSigner.Signature.HexToBytes());
+                                    var additionalSignerWitness = rawTx.Witnesses.FirstOrDefault(p => p.ScriptHash.ToAddress() == additionalSigner.Signer.Address);
+                                    if (additionalSignerWitness == null)
+                                    {
+                                        var additionalSignerScript = _context.Account.First(p => p.Address == additionalSigner.Signer.Address).GetScript();
+                                        rawTx.Witnesses = rawTx.Witnesses.Append(new Neo.Network.P2P.Payloads.Witness()
+                                        {
+                                            InvocationScript = null,
+                                            VerificationScript = additionalSignerScript
+                                        }).ToArray();
+                                    }
+                                    additionalSignerWitness = rawTx.Witnesses.FirstOrDefault(p => p.ScriptHash.ToAddress() == additionalSigner.Signer.Address);
+                                    additionalSignerWitness.InvocationScript = scriptBuilder.ToArray();
+                                    tx.RawData = rawTx.ToJson(ProtocolSettings.Load(ConfigHelper.AppSetting("Config"))).ToString();
+                                    _context.Update(additionalSigner);
+                                    _context.SaveChanges();
+                                }
+                            }
+                        }
+
+                        //签名数满足阈值时，其他用户的签名合并为多签账户的签名
+                        var otherSignResult = tx.SignResult.Where(p => p.Approved && tx.Account.Owners.Contains(p.Signer.Address)).Take(tx.Account.Threshold).ToList();
+                        if (otherSignResult?.Count == tx.Account.Threshold)
+                        {
+                            if (!rawTx.Witnesses.Any(p => p.VerificationScript.ToArray().ToHexString() == tx.Account.GetScript().ToHexString() && p.InvocationScript.Length > 0))
+                            {
+                                using ScriptBuilder scriptBuilder = new();
+                                otherSignResult.OrderBy(p => Neo.Cryptography.ECC.ECPoint.Parse(p.Signer.PublicKey, Neo.Cryptography.ECC.ECCurve.Secp256r1)).ForEach(p => scriptBuilder.EmitPush(p.Signature.HexToBytes()));
+                                rawTx.Witnesses.First(p => p.VerificationScript.ToArray().ToHexString() == tx.Account.GetScript().ToHexString()).InvocationScript = scriptBuilder.ToArray();
+                                tx.RawData = rawTx.ToJson(ProtocolSettings.Load(ConfigHelper.AppSetting("Config"))).ToString();
+                                _context.Update(tx);
+                                _context.SaveChanges();
+                            }
+                        }
+
+                        //发送交易
+                        //仅当签名完成且未发送，或已发送时间超过60秒，则广播交易
+                        if (rawTx.Witnesses.All(p => (p.VerificationScript.Length == 0 && p.InvocationScript.Length == 0) || p.VerificationScript.Length > 0 && p.InvocationScript.Length > 0) && tx.Status == Models.TransactionStatus.Signing ||
+                            tx.Status == Models.TransactionStatus.Executing && (DateTime.UtcNow - tx.ExecuteTime).TotalSeconds > 60)
+                        {
+                            try
+                            {
+                                var send = Helper.Client.SendRawTransactionAsync(rawTx).Result;
+                                tx.Status = Models.TransactionStatus.Executing;
+                                tx.ExecuteTime = DateTime.UtcNow;
+                            }
+                            catch (Exception e)
+                            {
+                                if (tx.Status != Models.TransactionStatus.Executed)
+                                {
+                                    _logger.Error($"发送交易时出错，TxId = {tx.Hash}, Exception: {e.Message}");
+                                    tx.Status = Models.TransactionStatus.Failed;
+                                    tx.FailReason = e.Message;
+                                    tx.ExecuteTime = DateTime.UtcNow;
+                                }
+                            }
                             _context.Update(tx);
                             _context.SaveChanges();
                         }
                     }
 
-                    //发送交易
-                    //仅当签名完成且未发送，或已发送时间超过60秒，则广播交易
-                    if (rawTx.Witnesses.All(p => (p.VerificationScript.Length == 0 && p.InvocationScript.Length == 0) ||p.VerificationScript.Length > 0 && p.InvocationScript.Length > 0) && tx.Status == Models.TransactionStatus.Signing ||
-                        tx.Status == Models.TransactionStatus.Executing && (DateTime.UtcNow - tx.ExecuteTime).TotalSeconds > 60)
+                    //后台任务二：检查交易是否上链并修改交易状态
+                    _context.Transaction.Where(p => p.Status == Models.TransactionStatus.Executing).ToList().ForEach(p =>
                     {
+                        var height = 0u;
                         try
                         {
-                            var send = Helper.Client.SendRawTransactionAsync(rawTx).Result;
-                            tx.Status = Models.TransactionStatus.Executing;
-                            tx.ExecuteTime = DateTime.UtcNow;
+                            height = Helper.Client.GetTransactionHeightAsync(p.Hash).Result;
                         }
                         catch (Exception e)
                         {
-                            if (tx.Status != Models.TransactionStatus.Executed)
-                            {
-                                _logger.Error($"发送交易时出错，TxId = {tx.Hash}, Exception: {e.Message}");
-                                tx.Status = Models.TransactionStatus.Failed;
-                                tx.FailReason = e.Message;
-                                tx.ExecuteTime = DateTime.UtcNow;
-                            }
+                            _logger.Error($"后台任务运行时种子节点连接失败 GetTransactionHeightAsync。{e.Message}");
                         }
-                        _context.Update(tx);
-                        _context.SaveChanges();
-                    }
-                }
+                        if (height > 0)
+                        {
+                            p.Status = Models.TransactionStatus.Executed;
+                            _context.Update(p);
+                            _context.SaveChanges();
+                        }
+                    });
 
-                //后台任务二：检查交易是否上链并修改交易状态
-                _context.Transaction.Where(p => p.Status == Models.TransactionStatus.Executing).ToList().ForEach(p =>
-                {
-                    var height = 0u;
+                    //后台任务三：检查交易是否过期并修改交易状态
+                    var blockCount = 0u;
                     try
                     {
-                        height = Helper.Client.GetTransactionHeightAsync(p.Hash).Result;
+                        blockCount = Helper.Client.GetBlockCountAsync().Result;
                     }
-                    catch (Exception)
+                    catch (Exception e)
                     {
-                        _logger.Error($"后台任务运行时种子节点连接失败 GetTransactionHeightAsync");
+                        _logger.Error($"后台任务运行时种子节点连接失败 GetBlockCountAsync。{e.Message}");
                     }
-                    if (height > 0)
+                    _context.Transaction.Where(p => p.Status == Models.TransactionStatus.Signing).ToList().ForEach(p =>
                     {
-                        p.Status = Models.TransactionStatus.Executed;
-                        _context.Update(p);
-                        _context.SaveChanges();
-                    }
-                });
+                        if (blockCount > p.ValidUntilBlock)
+                        {
+                            p.Status = Models.TransactionStatus.Expired;
+                            _context.Update(p);
+                            _context.SaveChanges();
+                        }
+                    });
 
-                //后台任务三：检查交易是否过期并修改交易状态
-                var blockCount = 0u;
-                try
-                {
-                    blockCount = Helper.Client.GetBlockCountAsync().Result;
+                    _context.SaveChanges();
                 }
-                catch (Exception)
+                catch (Exception e)
                 {
-                    _logger.Error($"后台任务运行时种子节点连接失败 GetBlockCountAsync");
+                    _logger.Error($"后台任务运行时出现未知错误。{e.Message} {e.StackTrace}");
                 }
-                _context.Transaction.Where(p => p.Status == Models.TransactionStatus.Signing).ToList().ForEach(p =>
-                {
-                    if (blockCount > p.ValidUntilBlock)
-                    {
-                        p.Status = Models.TransactionStatus.Expired;
-                        _context.Update(p);
-                        _context.SaveChanges();
-                    }
-                });
-
-                _context.SaveChanges();
-                _isRuning = false;
 
                 await Task.Delay(TimeSpan.FromSeconds(15), stoppingToken);
             }
